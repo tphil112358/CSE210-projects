4d652bd9ea81abcc23b1522d4cb32a72692e15d7
tphil112358 <tphil112358@gmail.com>
2024-01-20 23:37:49 -0700

    Finished prep 3 project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int x = 0;
+        while (x == 0)
+        {
+            Console.WriteLine("Try and guess the number: ");
+            string userInput = Console.ReadLine();
+            int guess = int.Parse(userInput);
+            if (guess == number)
+            {
+                Console.WriteLine("Correct! Nice guessing skills.");
+                x++;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("Higher.");
+            }
+        }
     }
 }
