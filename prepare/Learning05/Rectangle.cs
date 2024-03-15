4fce551dafc7e58584a8bdf6f94602771f11e9d6
tphil112358 <tphil112358@gmail.com>
2024-03-15 14:58:10 -0600

    Changed a few syntax things

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -2,7 +2,7 @@ public class Rectangle : Shape {
     private double _length;
     private double _width;
 
-    public Rectangle(string color, double length, double width) : base (color) {
+    public Rectangle(string color, double length,double width) : base(color) {
         _length = length;
         _width = width;
     }
